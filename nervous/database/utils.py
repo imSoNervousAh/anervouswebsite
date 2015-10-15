72f6ce32a2e6e5a1b074d530a892c847971338b2
RsEnts <360576743@qq.com>
2015-10-15 11:55:56 +0800

    添加了article测试数据

--- nervous/database/utils.py
+++ nervous/database/utils.py
@@ -5,6 +5,18 @@ from backend import *
 def build_test_db():
     Admin.create(username='wyl8899', password='xxxxxxxx', description='韦毅龙')
     Admin.create(username='ytl14', password='shenmegui', description='杨基龙')
+    
+    paras = {}
+    paras['wx_name'] = 'jiujingzixun'
+    d = getdata.get_dict('wx/opensearchapi/content_list', paras)
+    ind = (d['returnData'])['items']
+    for temp in ind:
+        temp['official_account_name'] = temp['name']
+        temp['description'] = temp['content']
+        temp['views'] = temp['readnum']
+        temp['likes'] = temp['likes']
+        backend.add_article(temp)
+    
 
     account = OfficialAccount.create(name='Lab Mu')
     Application.create(official_account=account, user_submit='FANG KUAI', status='not_submitted')
