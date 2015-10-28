1726defd0de51d7c030aeb19f1c09d834c0471f9
杨天龙 <yangtianlong111@gmail.com>
2015-10-29 03:18:58 +0800

    finish message

--- nervous/wechat/views.py
+++ nervous/wechat/views.py
@@ -64,7 +64,12 @@ def student(request):
 
     username = session.get_username(request)
 
-    return render(request, 'student/index.html', {'username': username})
+    official_account_id='112'
+
+    return render(request, 'student/index.html', {'username': username,
+                                                  'official_account_id': official_account_id,
+
+                                                  })
 
 
 def student_show_applications(request):
@@ -187,48 +192,13 @@ def admin_show_official_account_detail(request, id):
 class MessageCategory:
     All,ToStudent,ToAdmin=range(3)
 
-def message(request, id):
-    message1={
-        'realname':'ytl',
-        'category':'student',
-        'datetime':'2015.10.28',
-        'content':'学生申请怎么做?',
-    }
-    message2={
-        'realname':'haungdada',
-        'category':'admin',
-        'datetime':'2015.10.28',
-        'content':'不知道,董文飞凤凰网阿胶分哦覅 风味访问欧美佛问哦房氛围好奇偶访问欧非分雄风呢间哦'
-                  '你自己试试看吧',
-    }
-    message3={
-        'realname':'ytl',
-        'category':'student',
-        'datetime':'2015.10.28',
-        'content':'请问老师申请人联系方式怎么填？',
-    }
-    messages=[message1,message2,message3]
-    try:
-        official_account = backend.get_official_account_by_id(id)
-    except:
-        return to_notfound(request)
-    articles = backend.get_articles_by_official_account_id(id)
-
-    return render(request, 'administrator/message.html', {'account': official_account,
-                                                         'articles': articles,
-                                                         'article_count': len(articles),
-                                                         'username': session.get_username(request),
-                                                         'messages':messages,
-                                                         'MessageCategory':MessageCategory,
-                                                         })
-
 
 class MessageCategory:
     All, ToStudent, ToAdmin = range(3)
 
 
-def message(request, id):
-    messages = backend.get_messages(official_account_id = id)
+def admin_message(request, id):
+    print 'show admin_message'
     try:
         official_account = backend.get_official_account_by_id(id)
     except:
@@ -239,10 +209,18 @@ def message(request, id):
                                                           'articles': articles,
                                                           'article_count': len(articles),
                                                           'username': session.get_username(request),
-                                                          'messages': messages,
-                                                          'MessageCategory': MessageCategory,
                                                           'official_account_id':id,
                                                           })
+
+def message_detail_admin(request,id):
+    messages = backend.get_messages(official_account_id = id)
+    return render(request,'message/message.html',{'messages':messages,
+                                                   'MessageCategory': MessageCategory,
+                                                  'official_account_id':id,
+                                                  })
+
+def message_detail_student(request,id):
+    return message_detail_admin(request,id)
 
 
 # superuser
